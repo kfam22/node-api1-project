3fe7ce67f8207a288f801f9a12ddd367795bbb9a
Kayla Sade <sade@Kaylas-MacBook-Pro.local>
2022-02-07 21:47:31 -0500

    setup and first get request

--- api/server.js
+++ api/server.js
@@ -1,3 +1,39 @@
 // BUILD YOUR SERVER HERE
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+// imports
+const express = require('express');
+const user = require('./users/model');
+
+// instance of express app
+const server = express();
+
+// global middleware: by default, express cannot read request bodies, because put and post supply bodies as part of the request, this tells express how to interpret the request body, in this case as json
+server.use(express.json());
+
+// endpoints
+// GET - /api/users - Returns an array users.
+server.get('/api/users', (req, res)=>{
+    // pull data(users) from database
+    user.find()
+    .then(users=>{
+        res.json(users);
+    })
+    .catch(err =>{
+        res.status(500).json({
+            message: 'The users information could not be retrieved',
+            error: err.message
+        })
+    })
+})
+
+// POST - /api/users - Creates a user using the information sent inside the `request body`.  
+
+// GET - /api/users/:id - Returns the user object with the specified `id`
+
+// DELETE - /api/users/:id  - Removes the user with the specified `id` and returns the deleted user. 
+
+// PUT - /api/users/:id - Updates the user with the specified `id` using data from the `request body`. Returns the modified user
+
+
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}

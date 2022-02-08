231c963c085dc03f69614e0aaf8ac657941975fe
Kayla Sade <sade@Kaylas-MacBook-Pro.local>
2022-02-08 00:27:20 -0500

    delete and put requests complete

--- api/server.js
+++ api/server.js
@@ -64,8 +64,51 @@ server.get('/api/users/:id', (req, res)=>{
 })
 
 // DELETE - /api/users/:id  - Removes the user with the specified `id` and returns the deleted user. 
+server.delete('/api/users/:id', (req, res)=>{
+    let { id } = req.params;
+    user.remove(id)
+    .then(user =>{
+        if(user == null){
+            res.status(404).json({ message: 'The user with the specified ID does not exist' });
+            return;
+        }
+
+        res.status(200).json(user);
+    })
+    .catch(err =>{
+        res.status(500).json({ message: 'The user could not be removed', error: err.message})
+    })
+})
 
 // PUT - /api/users/:id - Updates the user with the specified `id` using data from the `request body`. Returns the modified user
+
+server.put('/api/users/:id', async (req, res)=>{
+    const { id } = req.params
+    try{
+        const actualUser = await user.findById(id)
+        if(!actualUser){
+            res.status(404).json({
+                message: 'The user with the specified ID does not exist'
+            })
+        } else {
+            if (!req.body.name || !req.body.bio){
+                res.status(400).json({
+                    message: 'Please provide name and bio for the user'
+                })
+            } else {
+                const updatedUser = await user.update(id, req.body)
+                res.status(200).json(updatedUser)
+            }
+        }
+    } catch(err){
+        res.status(500).json({
+            message: 'The user information could not be modified',
+            error: err.message
+        })
+
+    }
+
+})
 
 
 
